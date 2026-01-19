3e28a4d575b202ab5a23ed8bd141050ce3c61494
SiyuZhang-0426 <sylyuzhang@gmail.com>
2026-01-19 20:16:08 +0800

    update finetune

--- finetune/train_zero_t2v.sh
+++ finetune/train_zero_t2v.sh
@@ -1,31 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#SBATCH -p $vp
+#SBATCH --gres=gpu:4
 
-# Prevent tokenizer parallelism issues
-export TOKENIZERS_PARALLELISM=false
-
-# Model Configuration
+srun -p $vp --gres=gpu:4 apptainer exec --nv ~/ubuntu.sif bash -c "
+source ~/.bashrc && \
+conda activate cogvideox && \
+cd /mnt/petrelfs/zhangsiyu/4dgen/CogVideo/finetune && \
+export TOKENIZERS_PARALLELISM=false && \
 MODEL_ARGS=(
     --model_path "THUDM/CogVideoX1.5-5B"
     --model_name "cogvideox1.5-t2v"  # ["cogvideox-t2v"]
     --model_type "t2v"
     --training_type "sft"
-)
-
-# Output Configuration
+) && \
 OUTPUT_ARGS=(
-    --output_dir "/absolute/path/to/your/output_dir"
+    --output_dir "/mnt/petrelfs/zhangsiyu/4dgen/CogVideo/finetune_test_zero"
     --report_to "tensorboard"
-)
-
-# Data Configuration
+) && \
 DATA_ARGS=(
-    --data_root "/absolute/path/to/your/data_root"
+    --data_root "/mnt/petrelfs/zhangsiyu/4dgen/CogVideo/Disney-VideoGeneration-Dataset"
     --caption_column "prompt.txt"
     --video_column "videos.txt"
     --train_resolution "81x768x1360"  # (frames x height x width), frames should be 8N+1 and height, width should be multiples of 16
-)
-
-# Training Configuration
+) && \
 TRAIN_ARGS=(
     --train_epochs 10 # number of training epochs
     --seed 42 # random seed
@@ -35,32 +32,24 @@ TRAIN_ARGS=(
     --gradient_accumulation_steps 1
     --mixed_precision "bf16"  # ["no", "fp16"] Only CogVideoX-2B supports fp16 training
     ########################################################################
-)
-
-# System Configuration
+) && \
 SYSTEM_ARGS=(
     --num_workers 8
     --pin_memory True
     --nccl_timeout 1800
-)
-
-# Checkpointing Configuration
+) && \
 CHECKPOINT_ARGS=(
     --checkpointing_steps 10 # save checkpoint every x steps
     --checkpointing_limit 2 # maximum number of checkpoints to keep, after which the oldest one is deleted
     # --resume_from_checkpoint "/absolute/path/to/checkpoint_dir"  # if you want to resume from a checkpoint, otherwise, comment this line
-)
-
-# Validation Configuration
+) && \
 VALIDATION_ARGS=(
     --do_validation false  # ["true", "false"]
-    --validation_dir "/absolute/path/to/validation_set"
+    --validation_dir "/mnt/petrelfs/zhangsiyu/4dgen/CogVideo/Disney-VideoGeneration-Dataset"
     --validation_steps 20  # should be multiple of checkpointing_steps
     --validation_prompts "prompts.txt"
     --gen_fps 16
-)
-
-# Combine all arguments and launch training
+) && \
 accelerate launch --config_file accelerate_config.yaml train.py \
     "${MODEL_ARGS[@]}" \
     "${OUTPUT_ARGS[@]}" \
@@ -69,3 +58,6 @@ accelerate launch --config_file accelerate_config.yaml train.py \
     "${SYSTEM_ARGS[@]}" \
     "${CHECKPOINT_ARGS[@]}" \
     "${VALIDATION_ARGS[@]}"
+&& \
+echo "Training completed"
+"
---
+++ model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p $vp
+#SBATCH --gres=gpu:0
+
+srun -p $vp --gres=gpu:0 apptainer exec --nv ~/ubuntu.sif bash -c "
+source ~/.bashrc && \
+conda activate cogvideox && \
+cd /mnt/petrelfs/zhangsiyu/4dgen/CogVideo && \
+export http_proxy=http://zhangsiyu:3WqUQ2knmuHFeBXYcRKJ0rbA6EB3Jqg7Rx8bV1cjn2dtNSPZDBe9WIWbDtsn@10.1.20.50:23128/ && \
+export https_proxy=http://zhangsiyu:3WqUQ2knmuHFeBXYcRKJ0rbA6EB3Jqg7Rx8bV1cjn2dtNSPZDBe9WIWbDtsn@10.1.20.50:23128/ && \
+export HTTP_PROXY=http://zhangsiyu:3WqUQ2knmuHFeBXYcRKJ0rbA6EB3Jqg7Rx8bV1cjn2dtNSPZDBe9WIWbDtsn@10.1.20.50:23128/ && \
+export HTTPS_PROXY=http://zhangsiyu:3WqUQ2knmuHFeBXYcRKJ0rbA6EB3Jqg7Rx8bV1cjn2dtNSPZDBe9WIWbDtsn@10.1.20.50:23128/ && \
+export no_proxy=10.0.0.0/8,100.0.0.0/8,35.220.264.252/32,.pjlab.org.cn && \
+export HF_TOKEN=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 && \
+hf download zai-org/CogVideoX1.5-5B
+"
