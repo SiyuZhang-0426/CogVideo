5e40158fd36cee96141fbc1134d297603eeed625
Sylyu <sylyuzhang@gmail.com>
2026-01-20 21:57:46 +0800

    fix datasets

--- finetune/datasets/__init__.py
+++ finetune/datasets/__init__.py
@@ -3,10 +3,15 @@ from .i2v_dataset import I2VDatasetWithBuckets, I2VDatasetWithResize
 from .t2v_dataset import T2VDatasetWithBuckets, T2VDatasetWithResize
 
 
+class IterableDataset:
+    pass
+
+
 __all__ = [
     "I2VDatasetWithResize",
     "I2VDatasetWithBuckets",
     "T2VDatasetWithResize",
     "T2VDatasetWithBuckets",
     "BucketSampler",
+    "IterableDataset",
 ]
